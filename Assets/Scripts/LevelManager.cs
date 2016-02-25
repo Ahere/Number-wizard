910411e800b572cbc8bd2bbd0685a5e6c2a89a03
Ahere George Odongo <georgeahere@gmail.com>
2016-02-25 10:39:38 +0800

    Assets and the rest.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class LevelManager : MonoBehaviour
+
+{
+
+  public void LoadLevel(string name)
+   {
+
+	Debug.Log("New Level Load:" + name);
+   // Application.LoadLevel(name); 
+    SceneManager.LoadScene(name , LoadSceneMode.Single);
+     }
+
+   public void QuitRequest()
+   {
+    Debug.Log("Level quit");
+    Application.Quit();
+
+   }
+}
+
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	
+
+	public Text guessText; 
+
+	int max = 1000 ;
+	int min = 1 ;
+	int guess ; 
+
+	
+	void Start ()
+	{
+		
+     StartGame();
+	}
+
+  
+	
+	// Update is called once per frame
+   
+
+  
+    public void GuessHigher()
+ 		{
+        	min = guess;
+        	NextGuess();
+		}
+	public void GuessLower()
+		{  
+            max = guess;
+            NextGuess();
+      	}
+	public void GuessCorrect()
+		{  
+            
+            StartGame();
+      	}
+
+
+ 
+ 	void StartGame ()
+    	{  
+	       
+
+
+	       max += 1;
+      	   NextGuess();
+          
+		    //print("===================================");
+			//print("Welcome to NumberWizard");
+		    //print("Pick a number in your head but don't tell me");
+
+				
+			//print("the higest number you can pick is " + max + " and the lowest nuumber you can pick is " + min);
+			    
+		    //print("Is the number higher or lower than "+ guess +" ?" );
+			//print("Press Up = HIGHER, down arrow = LOWER and Enter = EQUAL TO");
+        
+    }
+
+
+  void NextGuess ()
+   {
+
+        //guess = (min + max)/2 ;
+        guess=Random.Range(min,max);
+        //guessText.text = guess.ToString();
+       ///print ("Higher or lower than "  + guess);
+       // print("Press Up = HIGHER, down arrow = LOWER and Enter = EQUAL TO");
+        guessText.text = guess.ToString();
+   }
+
+
+
+
+}
